13c1a03b55702f9cc337e039ae9ca99379bfa33d
codygeek01 <s474996633@gmail.com>
2023-10-26 02:07:04 +0530

    IO Done

---
+++ src/editor.rs
@@ -0,0 +1,120 @@
+use crate::terminal::Terminal;
+use termion::event::Key;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+pub struct Editor {
+    should_quit: bool,
+    terminal: Terminal,
+    cursor_position: Position
+}
+
+impl Editor {
+    pub fn run(&mut self) {
+        loop {
+            if let Err(err) = self.refresh_screen() {
+                die(&err);
+            }           
+
+            if self.should_quit {
+                break;
+            }
+            
+            if let Err(err) = self.process_keypress() {
+                die(&err);
+            }
+        }        
+    }
+
+    pub fn default() -> Self {
+        Self {
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to intialize terminal"),
+            cursor_position: Position { x: 0, y: 0 }
+        }
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::cursor_hide();
+        Terminal::cursor_position(&Position { x: 0, y: 0 });
+        if self.should_quit {            
+            Terminal::clear_screen();
+            println!("Goodbye.\r");            
+        } else {
+            self.draw_rows();
+            Terminal::cursor_position(&self.cursor_position);
+        }
+        Terminal::cursor_show();
+        // Flush everythin to the terminal
+        Terminal::flush()
+    }
+
+    fn draw_rows(&self) {
+        let height = self.terminal.size().height;
+        for row in 0..height-1 {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_message();
+            } else {
+                println!("~\r");
+            }
+        }
+    }
+
+    pub fn move_cursor(&mut self, key: Key) {
+        let Position{mut x, mut y} = self.cursor_position;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
+        match key {
+            Key::Up => y = y.saturating_sub(1),
+            Key::Down => {
+                if y < height {
+                    y = y.saturating_add(1);
+                }
+            },
+            Key::Left => x = x.saturating_sub(1),
+            Key::Right => {
+                if x < width {
+                    x = x.saturating_add(1);
+                }
+            },
+            Key::PageUp => y = 0,
+            Key::PageDown => y = height,
+            Key::Home => x = 0,
+            Key::End => x = width,
+            _ => ()
+        }
+        self.cursor_position = Position {x, y}
+    }
+
+    pub fn draw_welcome_message(&self) {
+        let mut welcome_message = format!("Iota Editor -- version {}\r", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len)/2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
+    }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = Terminal::read_key()?;
+        match pressed_key {
+          Key::Ctrl('q') => self.should_quit = true,
+          Key::Up | Key::Down | Key::Left | Key::Right| Key::PageUp | Key::PageDown | Key::Home | Key::End => self.move_cursor(pressed_key),
+          _ => (),  
+        }
+        Ok(())
+    }
+}
+
+fn die(e: &std::io::Error) {
+    panic!("{}", e);
+}
---
+++ src/terminal.rs
@@ -0,0 +1,70 @@
+use std::io::{self, stdout, Write};
+use termion::input::TermRead;
+use termion::event::Key;
+use termion::raw::{RawTerminal, IntoRawMode};
+use crate::Position;
+
+pub struct Size {
+    pub width: u16,
+    pub height: u16,
+}
+
+pub struct Terminal {
+    size: Size,
+    _stdout: RawTerminal<std::io::Stdout>
+}
+
+impl Terminal {
+    pub fn default() -> Result<Self, std::io::Error> {
+        let size = termion::terminal_size()?;
+        Ok(Self {
+            size: Size {
+                width: size.0,
+                height: size.1,
+            },
+            _stdout: stdout().into_raw_mode()?,
+        })
+    }
+
+    pub fn size(&self) -> &Size {
+        &self.size
+    }
+
+    pub fn clear_screen() {
+        print!("{}", termion::clear::All);
+    }
+
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+
+    pub fn read_key() -> Result<Key, std::io::Error>{
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
+    }
+
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
+    }
+
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
+    }
+
+    pub fn cursor_position(position: &Position) {
+        let Position{mut x, mut y} = position;
+        x = x.saturating_add(1);
+        y = y.saturating_add(1);
+        let x = x as u16;
+        let y = y as u16;
+        print!("{}", termion::cursor::Goto(x, y));
+    }
+
+}
